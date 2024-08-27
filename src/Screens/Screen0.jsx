3b83aa94bfde6c63a7c081f9b04dd04e926a570a
Sumitpanchal1993 <sumitpanchal08@gmail.com>
2024-08-27 19:20:31 +0530

    Chat added to Screen 2

---
+++ src/Screens/Screen0.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+function Screen0() {
+    const [username, setUsername] = useState('')
+
+  return (
+    <>
+    <div>
+        <div>
+            <label htmlFor=""></label>
+        </div>
+
+    </div>      
+    </>
+  )
+}
+
+export default Screen0
