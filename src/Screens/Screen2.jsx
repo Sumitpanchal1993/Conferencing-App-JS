bb12799f824c483effe1be4a15dee7566b6deec9
Sumitpanchal1993 <sumitpanchal08@gmail.com>
2024-08-30 13:39:29 +0530

    Navbar Updated

--- src/Screens/Screen2.jsx
+++ src/Screens/Screen2.jsx
@@ -1,45 +1,48 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./Screen2.css";
-import Navbar from '../Components/Navbar'
+import Navbar from "../Components/Navbar";
 import ChatScreen from "../Components/ChatScreen";
 import Camera from "../Sub Components/Camera";
 import PeoplesTile from "../Sub Components/PeoplesTile";
-import {attendees} from '../Static Data/PeoplesList'
-
+import { attendees } from "../Static Data/PeoplesList";
 
 function Screen2() {
-  const [isChatshown, setIsChatShown] = useState(false)  
-  const [isPeoplesList, setIsPeoplesList] = useState(false)
-  const [peoples, setPeoples] = useState([])
+  const [isChatshown, setIsChatShown] = useState(false);
+  const [isPeoplesList, setIsPeoplesList] = useState(false);
+  const [peoples, setPeoples] = useState([]);
 
-  useEffect(()=>{
-  setPeoples(attendees)
-  },[])
+  useEffect(() => {
+    setPeoples(attendees);
+  }, []);
 
   return (
     <>
       <div className="screen2-base">
         <div className="main-display">
           <div className="screen">
-            {
-              peoples.map((person)=>{
-                return(
-                  <div key={person.id} className="peoples"><Camera/></div>
-                )
-              })
-            }                      
+            {peoples.map((person) => {
+              return (
+                <div key={person.id} className="peoples">
+                  <Camera />
+                </div>
+              );
+            })}
           </div>
           <div className="side-Pannel">
-            {isChatshown ? <ChatScreen/>:peoples.map((people)=>{
-              return <PeoplesTile people ={people} />
-            })}
+            {isChatshown ? (
+              <ChatScreen />
+            ) : (
+              peoples.map((people) => {
+                return <PeoplesTile people={people} />;
+              })
+            )}
           </div>
         </div>
         <div className="controller">
-          <Navbar 
+          <Navbar
             setIsChatShown={setIsChatShown}
             setIsPeoplesList={setIsPeoplesList}
-          />             
+          />
         </div>
       </div>
     </>
@@ -47,18 +50,3 @@ function Screen2() {
 }
 
 export default Screen2;
-
-//experiment
-// Main Screen consist of : video, controller, chat, peoples
-// <button>
-//   <Link to="/screen3">Click Me</Link>
-// </button>
-// <h1>{storeData}</h1>
-
-// <button onClick={()=>{dispatch(increment())}}>
-//   increment
-// </button>
-// <button onClick={()=>{dispatch(decrement())}}>
-//   decrement
-// </button>
-
